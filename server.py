71b29fa098de7860951a958589b3221cc806a8fe
chethan <cnagesh@veritaz.ai>
2025-11-21 00:03:54 +0530

    fg

--- server.py
+++ server.py
@@ -1,17 +1,11 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from mangum import Mangum
 
 app = FastAPI()
 
-# Allow requests from any origin (React frontend)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # or specify your frontend URL
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
-
-# Simple GET endpoint
 @app.get("/api/data")
 async def get_data():
     return {"message": "Hello from FastAPI backend!"}
+
+# Serverless adapter
+handler = Mangum(app)
